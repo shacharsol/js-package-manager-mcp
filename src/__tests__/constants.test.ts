f3bad42942102aad0608d06820581964c76b7a90
Shachar Solomon <shacharsol@gmail.com>
2025-08-06 18:27:40 +0300

    bump: version v12.0.9

--- src/__tests__/constants.test.ts
+++ src/__tests__/constants.test.ts
@@ -13,7 +13,7 @@ describe('Constants', () => {
   describe('Version and Protocol', () => {
     it('should have valid version format', () => {
       expect(VERSION).toMatch(/^\d+\.\d+\.\d+$/);
-      expect(VERSION).toBe('12.0.8');
+      expect(VERSION).toBe('12.0.9');
     });
 
     it('should have valid MCP protocol version', () => {
